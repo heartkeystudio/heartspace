c7e904ee0f01bdd692dcafc2fc80ee65dd15e878
D3vAO <128839944+D3vAO@users.noreply.github.com>
2026-07-03 00:50:45 +0000

    Deploying to master from @ heartkeystudio/src_heartspace@27ebcf18d47bf34596df1fe8f59ab87089a6a5e9 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783039253|4304733';
+const CACHE_VERSION = '1783039832|4445982';
 /** @type {string} */
 const CACHE_PREFIX = 'HeartSpace-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
